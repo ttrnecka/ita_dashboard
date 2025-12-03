51fd4b4495026d27cc84bd5afed1f9c2a2ef8e4b
Tomas Trnecka <tomas.trnecka@cohesity.com>
2025-12-03 16:20:29 +0100

    iced 0.13.1 with composable temp

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use std::io::Write;
-
 use iced::{
     Alignment, Element, Length, Task, Theme, widget::{button, column, container, row, text}
 };
-
 
 mod temp;
 use temp::Graph;
@@ -31,59 +29,61 @@ struct MainApp {
     temp: Graph,
 }
 
-fn theme(_state: &MainApp) -> Theme {
-    Theme::TokyoNightStorm
-}
+impl MainApp {
+    fn theme(&self) -> Theme {
+        Theme::TokyoNightStorm
+    }
 
-fn update(state: &mut MainApp, message: Message) -> Task<Message> {
-    match message {
-        Message::Dashboard => {
-            state.selected = MenuItem::Dashboard;
-            Task::none()
-        }
-        Message::Temp(temp_message) => {
-            state.selected = MenuItem::Temp;
+    fn update(&mut self, message: Message) -> Task<Message> {
+        match message {
+            Message::Dashboard => {
+                self.selected = MenuItem::Dashboard;
+                Task::none()
+            }
+            Message::Temp(temp_message) => {
+                self.selected = MenuItem::Temp;
 
-            state.temp.update(temp_message).map(Message::Temp)
+                self.temp.update(temp_message).map(Message::Temp)
+            }
         }
     }
-}
 
-fn view(state: &'_ MainApp) -> Element<'_, Message> {
-    let menu = column![
-        button("Dashboard")
-            .width(Length::Fill)
-            .on_press(Message::Dashboard),
-        button("Temp")
-            .width(Length::Fill)
-            .on_press(Message::Temp(temp::Message::Load)),
-    ]
-    .spacing(2)
-    .padding(5)
-    .width(Length::Fixed(150.0));
+    fn view(&'_ self) -> Element<'_, Message> {
+        let menu = column![
+            button("Dashboard")
+                .width(Length::Fill)
+                .on_press(Message::Dashboard),
+            button("Temp")
+                .width(Length::Fill)
+                .on_press(Message::Temp(temp::Message::Load)),
+        ]
+        .spacing(2)
+        .padding(5)
+        .width(Length::Fixed(150.0));
 
-    // RIGHT SIDE READING PANE CONTENT
-    let content: Element<_> = match &state.selected {
-        MenuItem::Dashboard => {
-            text("Dashboard content here").into()
-        }
-        MenuItem::Temp => {
-            state.temp.view().map(Message::Temp)
-        }
-    };
+        // RIGHT SIDE READING PANE CONTENT
+        let content: Element<_> = match &self.selected {
+            MenuItem::Dashboard => {
+                text("Dashboard content here").into()
+            }
+            MenuItem::Temp => {
+                self.temp.view().map(Message::Temp)
+            }
+        };
 
-    let reading_pane = container(content)
-        .padding(20)
-        .width(Length::Fill);
+        let reading_pane = container(content)
+            .padding(20)
+            .width(Length::Fill);
 
-    // LAYOUT: menu left, content right
-    row![menu, reading_pane]
-        .align_y(Alignment::Start)
-        .into()
+        // LAYOUT: menu left, content right
+        row![menu, reading_pane]
+            .align_y(Alignment::Start)
+            .into()
+    }
 }
 fn main() -> iced::Result {
     let _ = std::io::stderr().flush();
 
-    iced::application("ITA Dashboard",update, view)
-    .theme(theme).run()
+    iced::application("ITA Dashboard",MainApp::update, MainApp::view)
+    .theme(MainApp::theme).run()
 }
